eae4cfa4ae6bd5e690257265d1b42d9273c2278a
yuuichi <lov.music.jaz@gmail.com>
2026-06-23 16:34:04 +0900

    chore: bump version to 0.10.6 (versionCode 76)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dd3boh.outertune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 75
-        versionName = "0.10.5"
+        versionCode = 76
+        versionName = "0.10.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
